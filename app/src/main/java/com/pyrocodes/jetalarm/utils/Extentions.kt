a7fa86ea295071539f3a5a2c457708f9daeac113
Akshay Sharma <fxn769@gmail.com>
2020-10-31 23:58:55 +0530

    timezones added

--- app/src/main/java/com/pyrocodes/jetalarm/utils/Extentions.kt
+++ app/src/main/java/com/pyrocodes/jetalarm/utils/Extentions.kt
@@ -1,11 +1,13 @@
 package com.pyrocodes.jetalarm.utils
 
 import android.os.Build
+import android.util.Log
 import android.view.View
 import android.view.Window
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.min
+import java.util.*
 
 
 const val oneMinuteRadians = Math.PI / 30
@@ -26,4 +28,15 @@ fun Window.setUpStatusNavigationBarColors(isLight: Boolean, hashCode: Int) {
             View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
         }
     }
+}
+
+fun getTimeZones() {
+    val timeZone = "timezone"
+    var default = TimeZone.getDefault()
+    Log.e(timeZone, "  default ${default.id} ${default.displayName} ${default.dstSavings}")
+    TimeZone.getAvailableIDs().forEach {
+        Log.e(timeZone, "->${it}")
+
+    }
+
 }
