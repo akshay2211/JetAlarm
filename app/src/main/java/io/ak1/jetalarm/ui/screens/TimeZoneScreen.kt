d3398a24fbd46b2ef44e27e19457ee69fb0ae2fc
Akshay Sharma <fxn769@gmail.com>
2021-11-03 10:37:59 +0530

    TimeZones Screen initiated

---
+++ app/src/main/java/io/ak1/jetalarm/ui/screens/TimeZoneScreen.kt
@@ -0,0 +1,26 @@
+package io.ak1.jetalarm.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+/**
+ * Created by akshay on 02/11/21
+ * https://ak1.io
+ */
+
+@Composable
+fun TimeZoneScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+    }
+}
