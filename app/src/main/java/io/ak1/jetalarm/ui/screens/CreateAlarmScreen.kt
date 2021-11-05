4535e0c8c4164451ea025ea516424c4419f6f99e
Akshay Sharma <fxn769@gmail.com>
2021-11-05 23:42:54 +0530

    CreateAlarmScreen.kt initiated

---
+++ app/src/main/java/io/ak1/jetalarm/ui/screens/CreateAlarmScreen.kt
@@ -0,0 +1,27 @@
+package io.ak1.jetalarm.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+/**
+ * Created by akshay on 05/11/21
+ * https://ak1.io
+ */
+
+@Composable
+fun CreateAlarmScreen(navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+    }
+}
